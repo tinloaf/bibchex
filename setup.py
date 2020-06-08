0fd48e13fa53c3ea82f7cd5513039435d93064d8
Lukas Barth <mail@tinloaf.de>
2020-06-08 21:07:36 +0200

    Add tests to setup.py

--- setup.py
+++ setup.py
@@ -6,21 +6,33 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'Readme.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
+requires = [
+    "aiohttp>=3.6.2",
+    "bibtexparser>=1.1.0",
+    "crossref-commons-reverse>=0.0.7.1",
+    "fuzzywuzzy>=0.18.0",
+    "isbnlib>=3.10.3",
+    "Jinja2>=2.11.1",
+    "nameparser>=1.0.6",
+    "python-dateutil>=2.8.1",
+    "ratelimit>=2.2.1"
+    ]
+
+test_requires = [
+    "pytest>=5.4.3",
+    "pytest-asyncio>=0.12.0",
+    "pytest-datadir-ng>=1.1.1"
+]
+
 setup(
     name="bibchex",
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(),
 
-    install_requires=["aiohttp>=3.6.2",
-                      "bibtexparser>=1.1.0",
-                      "crossref-commons-reverse>=0.0.7.1",
-                      "fuzzywuzzy==0.18.0",
-                      "isbnlib>=3.10.3",
-                      "Jinja2>=2.11.1",
-                      "nameparser>=1.0.6",
-                      "python-dateutil>=2.8.1",
-                      "ratelimit>=2.2.1"],
-
+    setup_requires=["pytest-runner>=5.2"],
+    install_requires=requires,
+    tests_require = requires + test_requires,
+    
     author="Lukas Barth",
     author_email="pypi@mbox.tinloaf.de",
     description="Check your BibTeX files for consistency and sanity.",
